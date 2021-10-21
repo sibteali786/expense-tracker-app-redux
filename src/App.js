58c7fa2d3d4660f28b52d4abb51be505b10a90a5
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-10-21 18:40:33 +0500

    made some small changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { Header, Balance, IncomeExpenses, AddTransaction } from "./components";
 function App() {
   return (
-    <div className="max-w-full h-full w-full">
-      <div className="flex justify-center items-center flex-col h-full w-full space-y-4 my-10">
+    <div className="max-w-full max-h-full h-full w-full bg-gray-100">
+      <div className="flex justify-center items-center flex-col h-full w-full space-y-4">
         <Header />
         <Balance />
         <IncomeExpenses />
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const IncomeExpenses = () => {
   return (
-    <div className="flex justify-center items-center space-x-20 bg-gray-50 p-10 shadow-md">
-      <div className="text-3xl p-5">
+    <div className="flex justify-center items-center space-x-20 bg-gray-50  shadow-md">
+      <div className="text-3xl pl-10 py-5">
         <h4>Income</h4>
         <p className="text-green-600 my-2">$400</p>
       </div>
 
-      <div className="text-3xl p-5">
+      <div className="text-3xl pr-10 py-5">
         <h4>Expense</h4>
         <p className="text-red-500 my-2">$100</p>
       </div>
