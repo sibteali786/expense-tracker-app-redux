a85f8b7a85136ef5799ff7fce21b1d828c65f32b
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-10-25 21:54:49 +0500

    redux state and management donw finally

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,6 @@
 import React from "react";
 
-const IncomeExpenses = ({ transactions }) => {
-  const amounts = transactions.map((transaction) =>
-    parseInt(transaction.amount, 10)
-  );
-  const income = amounts
-    .filter((item) => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-
-  const expense = (
-    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1
-  ).toFixed(2);
+const IncomeExpenses = ({ income, expense }) => {
   return (
     <div className="flex justify-center items-center space-x-20 bg-gray-50  shadow-md">
       <div className="text-3xl pl-10 py-5">
--- src/containers/IncomeExpensesContainer.js
+++ src/containers/IncomeExpensesContainer.js
@@ -2,7 +2,20 @@ import { connect } from "react-redux";
 import { IncomeExpenses } from "../components";
 
 export const mapStateToProps = (state) => {
-  return { transactions: state.transactions };
+  const transactions = state.transactions;
+  const amounts = transactions.map((transaction) =>
+    parseInt(transaction.amount, 10)
+  );
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+  return { income, expense };
 };
 
 export const IncomeExpensesContainer = connect(mapStateToProps)(IncomeExpenses);
